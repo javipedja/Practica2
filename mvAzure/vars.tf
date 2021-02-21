948e4d0995baf1ab2a124414d6b85d9afcc670b0
javipedja <42172249+javipedja@users.noreply.github.com>
2021-02-21 08:56:26 +0100

    reestructuración

--- mvAzure/vars.tf
+++ mvAzure/vars.tf
@@ -7,11 +7,11 @@ variable "location" {
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_D2_v2" # 3.5 GB, 1 CPU 
 }
 
 variable "vms" {
   type = list(string)
   description = "Maquinas virtuales del cluster de kubernetes"
-  default = ["master","worker01","nfs"]
+  default = ["master","worker01"]
 }
