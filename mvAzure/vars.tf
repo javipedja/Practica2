1a2b97c27a74f82bc3ae24765f873bb7e0974562
javipedja <42172249+javipedja@users.noreply.github.com>
2021-03-05 17:05:33 +0100

    comentario

--- mvAzure/vars.tf
+++ mvAzure/vars.tf
@@ -3,13 +3,6 @@ variable "location" {
   description = "Región de Azure donde crearemos la infraestructura"
   default = "West Europe"
 }
-
-variable "vm_size" {
-  type = string
-  description = "Tamaño de la máquina virtual"
-  default = "Standard_D2_v2" # 7 GB, 2 CPU 
-}
-
 variable "vm_sizes" {
   type = list(string)
   description = "Tamaño de la máquina virtual"
